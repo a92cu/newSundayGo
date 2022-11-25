0d8bd6f1a49783a5ae1bc9011056400ada04dc52
Krivsis <jimmm12312@gmail.com>
2022-11-25 17:31:16 +0800

    測試抓資料

--- pages/cartdetails/components/HotItem.jsx
+++ pages/cartdetails/components/HotItem.jsx
@@ -1,5 +1,17 @@
-import React from "react";
-import { runSQL } from "../../../lib/mysql";
+import React,{ useState } from "react";
+// import { runSQL } from "../../../lib/mysql";
+
+// 抓資料
+var fetchHotItem = async () => {
+    const response = await fetch("/api/HotItem").then(
+        response=>{
+            console.log(response);
+            return response;
+        }
+    );
+    const result = await response.json();
+};
+fetchHotItem();
 // 貓頭鷹輪播
 var $ = require("jquery");
 if (typeof window !== "undefined") {
@@ -30,7 +42,9 @@ const fontstyles = {
     "textDecoration": "none",
     "textAlign": "center",
 }
-export default function HotItem (props) {
+
+
+export default function HotItem () {
     return (
         <>
             <div className="cartcontainer">
@@ -43,74 +57,47 @@ export default function HotItem (props) {
                     autoplaySpeed={600} // 滑動速度
                     responsive={Responsive}
                 >
-                    <div className="item">
-                        <a href="/CartDetails/" style={fontstyles}>
-                            <Image width={280} height={180} padding={10}
-                                border-radius={20}
-                                src="/images/商品暫用圖/A02.jpg" alt="img" />
-                            <div>akjdlsjsaljdad</div>
-                        </a>
-                    </div>
-                    <div className="item">
-                        <a href="/CartDetails/" style={fontstyles}>
-                            <Image width={280} height={180} padding={10}
-                                border-radius={20}
-                                src="/images/商品暫用圖/A03.jpg" alt="img" />
-                            <div>askdjasldsjadlsd</div>
-                        </a>
-                    </div>
-                    <div className="item">
-                        <a href="/CartDetails/" style={fontstyles}>
-                            <Image width={280} height={180} padding={10}
-                                border-radius={20}
-                                src="/images/商品暫用圖/A04.jpg" alt="img" />
-                            <div>asjkdlsdj</div>
-                        </a>
-                    </div>
-                    <div className="item">
-                        <a href="/CartDetails/" style={fontstyles}>
-                            <Image width={280} height={180} padding={10}
-                                border-radius={20}
-                                src="/images/商品暫用圖/A05.jpg" alt="img" />
-                            <div>kasldjasldsjadla</div>
-                        </a>
-                    </div>
+                    {/* {item.forEach((i)=>(
+                        <div className="item">
+                            <a href={`/CartDetails/+${i.itemId}`} style={fontstyles}>
+                                <Image width={280} height={180} padding={10}
+                                    border-radius={20}
+                                    src={i.itemImgUrl} alt="img" />
+                                <div>{i.itemTitle}</div>
+                            </a>
+                        </div>
+                    ))} */}
                 </OwlCarousel>
             </div>
         </>
     )
 }
 
-export async function getStaticPaths(props) {
-    const sq1 = 'SELECT * FROM `itemimg` Left JOIN `item` ON itemimg.itemId=item.itemId WHERE itemLead=1 ORDER BY item.itemTotalStar DESC LIMIT 10;';
-    const {data,rows}= await runSQL(sq1);
-    console.log(data,rows);
-    const paths = data.map((item) => ({
-        params: { id: `${item.itemId}` },
-    }));
-    return {
-        paths,
-        fallback: false,
-    };
-}
-//頁面產生出來之後從params去找出特定需要的那一頁
-export async function getStaticProps({ params }) {
-    const sq1 = `SELECT * FROM item WHERE itemId = ${params.id}`;
-    const sq3 = `SELECT * FROM itemimg WHERE itemId = ${params.id}`;
-    // any是沒有定義的意思
-    const imgList= [];
+// export async function getStaticPaths(props) {
+    
+//     return {
+//         paths,
+//         fallback: false,
+//     };
+// }
+// //頁面產生出來之後從params去找出特定需要的那一頁
+// export async function getStaticProps({ params }) {
+//     const sq1 = `SELECT * FROM item WHERE itemId = ${params.id}`;
+//     const sq3 = `SELECT * FROM itemimg WHERE itemId = ${params.id}`;
+//     // any是沒有定義的意思
+//     const imgList= [];
 
-    const data = (await runSQL(sq1))[0];
-    const imgListRaw = await runSQL(sq3);
-    //forEach是在轉格式,原本出來是database物件
-    imgListRaw.forEach((item) => {
-        imgList.push({ ...item });
-    });
-    //把要的資料拿出來
-    return {
-        props: {
-            ...data,
-            imgList,
-        },
-    };
-}
+//     const data = (await runSQL(sq1))[0];
+//     const imgListRaw = await runSQL(sq3);
+//     //forEach是在轉格式,原本出來是database物件
+//     imgListRaw.forEach((item) => {
+//         imgList.push({ ...item });
+//     });
+//     //把要的資料拿出來
+//     return {
+//         props: {
+//             ...data,
+//             imgList,
+//         },
+//     };
+// }
