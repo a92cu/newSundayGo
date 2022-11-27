26fb945c8a95eb5e17f711d43098002d95fe19ce
Krivsis <jimmm12312@gmail.com>
2022-11-27 14:31:55 +0800

    抓資料

--- pages/cartdetails/components/HotItem.jsx
+++ pages/cartdetails/components/HotItem.jsx
@@ -1,17 +1,5 @@
-import React,{ useState } from "react";
+import React,{ useEffect,useState } from "react";
 // import { runSQL } from "../../../lib/mysql";
-
-// 抓資料
-var fetchHotItem = async () => {
-    const response = await fetch("/api/HotItem").then(
-        response=>{
-            console.log(response);
-            return response;
-        }
-    );
-    const result = await response.json();
-};
-fetchHotItem();
 // 貓頭鷹輪播
 var $ = require("jquery");
 if (typeof window !== "undefined") {
@@ -21,7 +9,8 @@ if (typeof window !== "undefined") {
 import "owl.carousel/dist/assets/owl.carousel.css";
 import "owl.carousel/dist/assets/owl.theme.default.css";
 import dynamic from "next/dynamic";
-import Image from "next/image";
+import { setSeconds } from "date-fns";
+
 const OwlCarousel = dynamic(() => import("react-owl-carousel"), {
     ssr: false,
 });
@@ -42,13 +31,35 @@ const fontstyles = {
     "textDecoration": "none",
     "textAlign": "center",
 }
-
+const imgstyles={
+    "width":"280px",
+    "height":"180px",
+    "padding":"10px",
+    "margin":"auto",
+    "display": "block",
+    "borderRadius":"20",
+    "object-fit": "cover",
+    "object-position": "center"
+}
 
 export default function HotItem () {
-    return (
+    
+    var [HotItemData, setHotItemData] = useState([]);
+    function fetchData() {
+        return (fetch("/api/Cart/HotItem",{next:{revalidate: 10}})
+                .then((response) => response.json())
+                .then((dataresult) => setHotItemData(dataresult.data))
+                )
+    }
+    // 使用component渲染tsx會有一個問題，在你抓資料的同時他在渲染畫面，導致資料進不去畫面
+    // 所以在使用 useEffect 把資料放進 return 的時候，先讓useEffect走setSeconds讓資料跑完
+    // 再去跑return
+    useEffect(() => {setSeconds( fetchData() ,10),[]})
+    return(
         <>
             <div className="cartcontainer">
                 <div className="toMoreHot">更多推薦商品</div>
+                <span>{HotItemData.forEach((i)=>{i})}</span>
                 <OwlCarousel
                     loop={true} // 循環播放
                     nav={false} // 顯示點點
@@ -57,47 +68,16 @@ export default function HotItem () {
                     autoplaySpeed={600} // 滑動速度
                     responsive={Responsive}
                 >
-                    {/* {item.forEach((i)=>(
-                        <div className="item">
+                    {HotItemData.map((i,index)=>
+                        <div className="item" key={index}>
                             <a href={`/CartDetails/+${i.itemId}`} style={fontstyles}>
-                                <Image width={280} height={180} padding={10}
-                                    border-radius={20}
-                                    src={i.itemImgUrl} alt="img" />
+                                <img src={i.itemImgUrl} style={imgstyles} alt="img" />
                                 <div>{i.itemTitle}</div>
                             </a>
                         </div>
-                    ))} */}
+                    )}
                 </OwlCarousel>
             </div>
         </>
     )
 }
-
-// export async function getStaticPaths(props) {
-    
-//     return {
-//         paths,
-//         fallback: false,
-//     };
-// }
-// //頁面產生出來之後從params去找出特定需要的那一頁
-// export async function getStaticProps({ params }) {
-//     const sq1 = `SELECT * FROM item WHERE itemId = ${params.id}`;
-//     const sq3 = `SELECT * FROM itemimg WHERE itemId = ${params.id}`;
-//     // any是沒有定義的意思
-//     const imgList= [];
-
-//     const data = (await runSQL(sq1))[0];
-//     const imgListRaw = await runSQL(sq3);
-//     //forEach是在轉格式,原本出來是database物件
-//     imgListRaw.forEach((item) => {
-//         imgList.push({ ...item });
-//     });
-//     //把要的資料拿出來
-//     return {
-//         props: {
-//             ...data,
-//             imgList,
-//         },
-//     };
-// }
