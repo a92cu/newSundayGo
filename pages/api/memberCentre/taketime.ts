a33e275790c23e900c7ade086f0de57f2a30140e
Krivsis <jimmm12312@gmail.com>
2022-12-02 17:52:22 +0800

    增加api

---
+++ pages/api/memberCentre/taketime.ts
@@ -0,0 +1,47 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { runSQL } from "../../../lib/mysql";
+
+export default async function userHandler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const {
+    query: { id },
+    method,
+  } = req;
+  switch (method) {
+    case "GET":
+      try {
+        // 刪除商品內容
+        const sq1 = `SELECT * FROM usertable WHERE userId="u123456789"`;
+        const data= await runSQL(sq1);
+        res.status(200).json({data});
+      } catch (error) {
+        res.status(500);
+      }
+    case "PUT":
+    try {
+      // 刪除商品內容
+      const sq1 = `UPDATE usertable SET userLoginEventTime="2022-12-02" WHERE userId="u123456789"`;
+      const data= await runSQL(sq1);
+      // console.log(req)
+      res.status(200).json({data});
+    } catch (error) {
+      res.status(500);
+    }
+    case "POST":
+    try {
+      // 刪除商品內容
+      const sq1 = `UPDATE usertable SET userLoginEventTime="2022-12-02" WHERE userId="u123456789"`;
+      const data= await runSQL(sq1);
+      // console.log(req)
+      res.status(200).json({data});
+    } catch (error) {
+      res.status(500);
+    }
+      break;    
+    default:
+      res.setHeader("Allow", ["GET", "PUT"]);
+      res.status(405).end(`Method ${method} Not Allowed`);
+  }
+}
