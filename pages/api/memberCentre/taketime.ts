552d4573184f87d22d7f1c63fdf8a4fd0611d821
Krivsis <jimmm12312@gmail.com>
2022-12-05 16:39:02 +0800

    更改API

--- pages/api/memberCentre/taketime.ts
+++ pages/api/memberCentre/taketime.ts
@@ -9,6 +9,7 @@ export default async function userHandler(
     query: { id },
     method,
   } = req;
+  console.log(req.body.date)
   switch (method) {
     case "GET":
       try {
@@ -19,27 +20,30 @@ export default async function userHandler(
       } catch (error) {
         res.status(500);
       }
+    break;
     case "PUT":
     try {
       // 刪除商品內容
-      const sq1 = `UPDATE usertable SET userLoginEventTime="2022-12-02" WHERE userId="u123456789"`;
-      const data= await runSQL(sq1);
+      const sq2 = `UPDATE usertable SET userLoginEventTime="${req.body.date}" WHERE userId="u123456789"`;
+      const sq3 = `INSERT INTO discountcoupon ( userId, coupon , couponName, couponStartTime,couponUse ) VALUES ("u123456789", 0.9 ,"九折","${req.body.discountdate}",0)`;
+      const data= await runSQL(sq2);
+      const data2=await runSQL(sq3);
       // console.log(req)
-      res.status(200).json({data});
+      res.status(200).json({data,data2});
     } catch (error) {
       res.status(500);
     }
-    case "POST":
-    try {
-      // 刪除商品內容
-      const sq1 = `UPDATE usertable SET userLoginEventTime="2022-12-02" WHERE userId="u123456789"`;
-      const data= await runSQL(sq1);
-      // console.log(req)
-      res.status(200).json({data});
-    } catch (error) {
-      res.status(500);
-    }
-      break;    
+    // case "POST":
+    // try {
+    //   刪除商品內容
+    //   const sq1 = `UPDATE usertable SET userLoginEventTime="2022-12-02" WHERE userId="u123456789"`;
+    //   const data= await runSQL(sq1);
+    //   console.log(req)
+    //   res.status(200).json({data});
+    // } catch (error) {
+    //   res.status(500);
+    // }
+    break;    
     default:
       res.setHeader("Allow", ["GET", "PUT"]);
       res.status(405).end(`Method ${method} Not Allowed`);
