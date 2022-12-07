a6c69fda73cf8027e217e4e8d721b1b2452b8505
Krivsis <jimmm12312@gmail.com>
2022-12-07 14:03:01 +0800

    將減少商品導致減少總價的bug修正

--- pages/cartdetails/components/CartItem_item.jsx
+++ pages/cartdetails/components/CartItem_item.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-function Product({ 
+function Product({
     id,
     date,
     count,
@@ -8,49 +8,48 @@ function Product({
     itemTitle,
     itemPrice,
     itemImgUrl,
-    onCalculate,
-    onDoAllClick}){
-    const [ quantity, setQuantity] = useState(0);  //  quantity 預設值= 0
+    onCalculate }) {
+    const [quantity, setQuantity] = useState(0);  //  quantity 預設值= 0
     // 當input 被點到會觸發...
-    let gotChange=(e)=>{
-        if(e.target.checked){
-            onDoAllClick(e.target.checked)
-            onCalculate(quantity*itemPrice);
-        }else{
-            onDoAllClick(e.target.checked)
-            if(-quantity*itemPrice>=0){
-                onCalculate(-quantity*itemPrice);
-            }
+    let gotChange = (e) => {
+        if (e.target.checked) {
+            onCalculate(quantity * itemPrice);
+        } else {
+            onCalculate(-quantity * itemPrice);
         }
     }
-    ;
-    useEffect(()=>{
+        ;
+    useEffect(() => {
         setQuantity(parseInt(count));
-        if(check){
-            console.log(document.getElementsByClassName("liCheck")) 
+        if (check) {
+            console.log(document.getElementsByClassName("liCheck"))
         }
     }
-    ,[])
-    
+        , [])
+
     const increment = (e) => {
-        let inputCheck=e.target.parentElement.parentElement.parentElement.firstChild.firstChild.checked
+        let inputCheck = e.target.parentElement.parentElement.parentElement.firstChild.firstChild.checked
         setQuantity(quantity + 1);   // 可以想成 quantity = quantity + 1
-        if(inputCheck){
+        if (inputCheck) {
             onCalculate(itemPrice);
         }
     };
     const decrement = (e) => {
-        let inputCheck=e.target.parentElement.parentElement.parentElement.firstChild.firstChild.checked
-        if ( quantity > 0 ){
-              setQuantity(quantity - 1);    // quantity = quantity - 1
+        let inputCheck = e.target.parentElement.parentElement.parentElement.firstChild.firstChild.checked
+        if (quantity > 0) {
+            setQuantity(quantity - 1);    // quantity = quantity - 1
         }
-        if(inputCheck){
-            if(quantity*itemPrice>=0){
-                onCalculate(-itemPrice);
+        if (quantity == 0) {
+            setQuantity(0);
+        }else{
+            if (inputCheck) {
+                if (quantity * itemPrice >= 0) {
+                    onCalculate(-itemPrice);
+                }
             }
         }
     };
-    let deleteMe=(e)=>{
+    let deleteMe = (e) => {
         e.target.parentElement.parentElement.remove()
     }
     return (
@@ -58,7 +57,7 @@ function Product({
             <div className="carHeader carBody">
                 <div className="carChecked">
                     {/* <!-- 個別核取方塊 --> */}
-                    <input type="checkbox" name="subItem" className="liCheck" onChange={gotChange} value={id}/>
+                    <input type="checkbox" name="subItem" className="liCheck" onChange={gotChange} value={id} />
                 </div>
                 <div className="carDetail">
                     <a href={`/item/${id}`}><img src={itemImgUrl} /></a>
@@ -76,7 +75,7 @@ function Product({
                 </div>
                 {/* <!-- 商品個別小計 --> */}
                 <div className="amount">
-                    <span className="itemTotal">{quantity*itemPrice}</span>
+                    <span className="itemTotal">{quantity * itemPrice}</span>
                 </div>
                 <div className="operate">
                     <i id="deleteOut" className="fa fa-trash-o fa-2x" aria-hidden="true" onClick={deleteMe}></i>
